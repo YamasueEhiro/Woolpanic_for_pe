2bb955f3cfb88926b4ac663848254f27373a6945
{YamasueEhiro} <{yamasue.ehiro@gmail.com}>
2017-03-30 16:09:13 +0900

    持ち上げるための

---
+++ copy.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem
+rem このバッチの説明
+rem
+
+rem 設定事項
+set HOGE="変数の値"
+
+rem このバッチが存在するフォルダをカレントに
+pushd %0\..
+cls
+for /F "usebackq" %%i in (`dir /b `) do (
+      xcopy %~dp0%%i /y "D:\WpSystem\S-1-5-21-2553201836-958739937-4288200835-1001\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\gFkAAJXZBwA=\behavior_packs\Equipments\"%%i
+    echo %~dp0%%i
+    )
+ rem    xcopy %~p0\.."%%i" "D:\WpSystem\S-1-5-21-2553201836-958739937-4288200835-1001\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\gFkAAJXZBwA=\behavior_packs"
+
+ rem )
+
+pause
+exit
+
+rem xcopy D:\WpSystem\S-1-5-21-2553201836-958739937-4288200835-1001\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\gFkAAJXZBwA=\behavior_packs
